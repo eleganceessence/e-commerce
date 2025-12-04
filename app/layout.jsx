4faa56d45fd1e2e8edf81ef00d2d4792bab71eea
Spark16x <spark2009971@gmail.com>
2025-12-04 07:31:28 +0530

    update

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import SplashScreen from "@/components/SplashScreen.jsx";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <SplashScreen/>
         {children}
         </ThemeProvider>
       </body>
