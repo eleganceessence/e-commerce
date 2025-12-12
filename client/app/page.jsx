c5f2988cb6d2177f7fbe9cf5d3bf5797d12bb9b7
Spark16x <spark2009971@gmail.com>
2025-12-12 14:45:06 +0530

    update page.jsx

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Nav from '@/components/nav';
 export default function Home() {
   return (
-    <div className="flex items-center justify-center">
+    <div className="">
       <Nav/>
       <h1 className="">
       Elegance essence
