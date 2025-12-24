0bbdd7b98a37ab96aa7c19aee980d705cf6d35d1
Spark16x <spark2009971@gmail.com>
2025-12-24 22:02:26 +0530

    update db.js

--- server/db.js
+++ server/db.js
@@ -2,6 +2,6 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 export const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL,
+  connectionString: 'postgresql://neondb_owner:npg_av1RNBHybE4A@ep-gentle-frost-a1jecscc-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
   ssl: { rejectUnauthorized: false },
 });
